0a9ab9bc417fea675eb026f8dd79030feb2e4e50
Erik García Rosa <erikgr10@gmail.com>
2021-06-21 11:11:34 +0200

    first commit

---
+++ database/migrations/2021_06_15_094002_create_children_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChildrenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('surname');
+            $table->integer('age');
+            $table->string('group');
+            $table->boolean('hasPayed');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('children');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<link href="{{ asset('stylesheets/welcome.css') }}" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Inici de sessió - Minuapp</title>
+    </head>
+    <body>
+        <div id=gridContainer>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div id=loginContainer>
+                <div id=loginBox>login</div>
+            </div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
+            <div>9</div>
+        </div>
+    </body>
+</html>
